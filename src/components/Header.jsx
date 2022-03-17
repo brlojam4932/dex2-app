ac9b8f4558031dc923809f45eab7f01bee77f02a
brlojam4932 <36285465+brlojam4932@users.noreply.github.com>
2022-03-17 13:37:48 -0700

    Netlify
    Cleaning file...small changes in explaining the product to users.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,8 +9,9 @@ function Header() {
           <h1 className="text-xl font-semibold text-info text-left">
             Interact with ERC20 Smart Contracts and DEX UI
           </h1>
-          <p><small className="text-muted">Read from a smart contract, approve, transfer, transfer from and recieve transaction messages from the blockchain.</small> </p>
+          <p><small className="text-muted">Read and write from any ERC20 Token smart contract on the Robsten testnet blockchain. Use approve, transfer, transfer from and receive event messages from the blockchain.</small> </p>
           <br />
+          <a className="nav-link text-info" href='https://ropsten.etherscan.io/token/0xe4b6351dc44f54e5cbbbe9008f06fa253001bcfb'>Link to my ERC20 RETK token contract </a>
         </main>
       </div>
     </div>
